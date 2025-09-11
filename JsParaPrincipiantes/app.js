243f85fab7c8b85b2b663d23dab661d58532283a
gabrielvalenciaolvera-glitch <gabrielvalenciaolvera@gmail.com>
2025-09-11 08:42:01 -0600

    Tipos de Variables en JavaScript

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -5,6 +5,8 @@ document.write("<h1>hello word</h1>")
 "hello Word"//String
 'Hello Word'//String
 
+
+console.log({"username":"ryan" "score":70.4})
 1000000// number
 -2,3
 
@@ -29,5 +31,23 @@ false
     "username": 'joe',
     "score": 0.4,
     "hours": 1,
-    "proffesional": true
+    "proffesional": false
+
 }
+//variable
+
+ var nameuser = "jhon";
+ let lastname = "carter";
+
+nameuser = "pepe";
+
+const PI = 3.1415;
+
+
+
+console.log(PI);
+ console.log(lastname);
+
+//camelcase
+
+let userName = "gordon";
