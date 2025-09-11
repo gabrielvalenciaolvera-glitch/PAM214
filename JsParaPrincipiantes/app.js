4218cfa944c1770123d896dd0a1c31eb758bde0c
gabrielvalenciaolvera-glitch <gabrielvalenciaolvera@gmail.com>
2025-09-10 18:23:33 -0600

    Intruduccion a JS, conceptos basicos

---
+++ JsParaPrincipiantes/app.js
@@ -0,0 +1,33 @@
+document.write("<h1>hello word</h1>")
+
+//tipos de datos
+
+"hello Word"//String
+'Hello Word'//String
+
+1000000// number
+-2,3
+
+// boolean
+true
+false
+
+//array
+['joe','ryan','marthe']
+[1,2,3]
+[true,false,true,false]
+
+// object
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+}
+{
+    "username": 'joe',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": true
+}
