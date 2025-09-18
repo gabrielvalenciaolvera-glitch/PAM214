98bfc9933dae3249d7dabc7e5ec4fe38dad88861
gabrielvalenciaolvera-glitch <gabrielvalenciaolvera@gmail.com>
2025-09-17 18:39:37 -0600

    Funciones básicas a Funciones flecha

---
+++ Practica1/FuncionFlecha.js
@@ -0,0 +1,12 @@
+
+/*
+function cuadrado(numero){
+    return numero * numero;
+}
+*/
+
+let cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(2));
+console.log(cuadrado(4));
+console.log(cuadrado(3));
