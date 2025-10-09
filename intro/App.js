346606666bb3f24e21281f78c18657745afe2a9b
gabrielvalenciaolvera-glitch <gabrielvalenciaolvera@gmail.com>
2025-10-09 17:51:10 -0600

    Update App.js

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,25 @@
+//1. imports: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react';
 
+//2. Mainimports: Zona de componentes
 export default function App() {
+
+const[contador, setContador]= useState(0);  
+
   return (
     <View style={styles.container}>
-      <Text>Hola mundo Web</Text>
+      <Text>Contador: {contador} </Text>
+      <Button title='Agregar' onPress={()=> setContador(contador+1)}/>
+      <Button title="Decrementar" onPress={()=> setContador(contador-1)}/>
+      <Button title="Reiniciar" onPress={()=> setContador(0)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//3. Estilos: Zona estetica y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
